58f3286058d4db5d20122ec68c785a7551bea721
vmyaki <wonka.vm@gmail.com>
2017-10-20 14:05:28 +0200

    subida a git

---
+++ src/main.java
@@ -0,0 +1,28 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// Declaramos variables
+					int num=50;
+				
+				//Bucle While
+				System.out.println("Bucle con While");	
+				while(num>=1)
+				{
+					System.out.println(num);
+					num=num-1;
+				}
+				
+				num=50;
+				//Bucle Do While
+				System.out.println("Bucle con Do While");
+				do {
+
+					System.out.println(num);
+					num=num-1;
+				}
+				while(num>=1);
+	}
+
+}
+
